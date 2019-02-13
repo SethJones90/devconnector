afc5524d5443f2bcdd03448bba2eddfb442ef0e8
Seth <seth@seths-mbp.attlocal.net>
2019-02-12 22:30:04 -0600

    created basic user registration resource

--- models/User.js
+++ models/User.js
@@ -17,7 +17,6 @@ const UserSchema = new Schema({
   },
   avatar: {
     type: String,
-    required: true
   },
   date: {
     type: Date,
@@ -26,4 +25,4 @@ const UserSchema = new Schema({
 
 });
 
-module.exports = User = mongoose.model('users', User);
+module.exports = User = mongoose.model('users', UserSchema);
