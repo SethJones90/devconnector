8ee8cf4fd39ee80b2d84b8fc0ed23d0c7441299f
Seth <seth@seths-mbp.attlocal.net>
2019-02-26 20:56:59 -0600

    created index, delete, and show routes

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -6,6 +6,9 @@ const passport = require('passport');
 // Post model
 const Post = require('../../models/Post');
 
+// Profile model
+const Profile = require('../../models/Profile');
+
 // Validation
 const validatePostInput = require('../../validation/post');
 
@@ -14,10 +17,30 @@ const validatePostInput = require('../../validation/post');
 // @access  Public route
 router.get('/test', (req, res) => res.json({msg: "Posts works"}));
 
+
+// @route   GET api/posts
+// @desc    Get posts
+// @access  Public route
+router.get('/', (req, res) => {
+  Post.find()
+    .sort({ date: -1 })
+    .then(posts => res.json(posts))
+    .catch(err => res.status(404).json({nopostsfound: "No posts found with that ID"}));
+});
+
+// @route   GET api/posts/:id
+// @desc    Get post
+// @access  Public route
+router.get('/:id', (req, res) => {
+  Post.findById(req.params.id)
+    .then(post => res.json(post))
+    .catch(err => res.status(404).json({nopostfound: "No post found with that ID"}));
+});
+
+
 // @route   POST api/post
 // @desc    Create posts
 // @access  Private route
-
 router.post('/', passport.authenticate('jwt', {session: false}), (req, res) => {
     const {errors, isValid} = validatePostInput(req.body);
 
@@ -35,6 +58,26 @@ router.post('/', passport.authenticate('jwt', {session: false}), (req, res) => {
     })
 
     newPost.save().then(post => res.json(post));
+});
+
+
+// @route   DELETE api/posts/:id
+// @desc    Delete post
+// @access  Private route
+router.delete('/:id', passport.authenticate('jwt', {session: false}), (req, res) => {
+  Profile.findOne({ user: req.user.id})
+    .then(profile => {
+      Post.findById(req.params.id)
+        .then(post => {
+          //Check for post owner
+          if (post.user.toString() !== req.user.id) {
+            return res.status(401).json({ notauthorized: "User not authorized" });
+          }
+          //Delete
+          post.remove().then(() => res.json({ success: true }))     
+        })
+        .catch(err => res.status(404).json({ postnotfound: "No post found"}))
+    })
 });
 
 
