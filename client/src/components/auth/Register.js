f80efafb4194fe08b4a5f67204acfa9248fe2558
Seth <seth@seths-mbp.attlocal.net>
2019-03-07 07:27:01 -0600

    created redux actions/reducers, errors, and moved register logic into redux

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
-import Axios from 'axios';
-import classnames from 'classnames'
+import PropTypes from 'prop-types';
+import { withRouter } from 'react-router-dom';
+import classnames from 'classnames';
+import { connect } from 'react-redux';
+import { registerUser } from '../../actions/authActions'
 
 class Register extends Component {
 
@@ -13,6 +16,12 @@ class Register extends Component {
       password2: '',
       errors: {}
     };
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if(nextProps.errors) {
+      this.setState({  errors: nextProps.errors });
+    }
   }
 
   onChange = (e) => {
@@ -29,17 +38,14 @@ class Register extends Component {
       password2: this.state.password2
     }
 
-    console.log(newUser);
-    Axios.post('api/users/register', newUser)
-      .then(res => console.log(res.data))
-      .catch(err => this.setState({errors: err.response.data}));
+    this.props.registerUser(newUser, this.props.history);
   }
 
   render() {
 
     // is equal to const errors = this.state.errors;
     const { errors } = this.state;
-
+    
     return (
       <div className="register">
         <div className="container">
@@ -107,4 +113,15 @@ class Register extends Component {
   }
 }
 
-export default Register;
+Register.propTypes = {
+  registerUser: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired,
+  errors: PropTypes.object.isRequired
+}
+
+const mapStateToProps = (state) => ({
+  auth: state.auth,
+  errors: state.errors
+})
+
+export default connect(mapStateToProps, { registerUser })(withRouter(Register));
